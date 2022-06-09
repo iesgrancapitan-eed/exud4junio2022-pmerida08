75662a82a33aefcdefdb0675aff958f4ae43d7fe
pmerida <pablomerida03@gmail.com>
2022-06-09 09:36:54 +0200

    docs: comentarios JavaDoc

---
+++ com/examen/junio/Bike.java
@@ -0,0 +1,68 @@
+package com.examen.junio;
+
+/**
+ * Representa la clase Bike que hereda de la clase Vehicle
+ * @author Pablo
+ *
+ */
+class Bike extends Vehicle {  
+  @Override
+public String toString() {
+	return "Bike [speed=" + speed + ", gear=" + gear + "]";
+}
+  
+}
+
+//class Bike implements Vehicle {
+//    
+//  int speed;
+//  int gear;
+//
+//  @Override
+//  public void changeGear(int newGear){
+//        
+//      gear = newGear;
+//  }
+//    
+//  @Override
+//  public void speedUp(int increment){
+//        
+//      speed = speed + increment;
+//  }
+//
+// @Override
+//  public void applyBrakes(int decrement){
+//        
+//      speed = speed - decrement;
+//  }
+//    
+//  public void printStates() {
+//      System.out.println("speed: " + speed
+//          + " gear: " + gear);
+//  } 
+//}
+
+//class GFG {
+//    
+//  public static void main (String[] args) {
+//    
+//      // creating an inatance of Bike
+//      // doing some operations
+//      Bike bicycle = new Bike();
+//      bicycle.changeGear(2);
+//      bicycle.speedUp(3);
+//      bicycle.applyBrakes(1);
+//        
+//      System.out.println("Bike present state :");
+//      bicycle.printStates();
+//        
+//      // creating instance of the bike.
+//      Bike bike = new Bike();
+//      bike.changeGear(1);
+//      bike.speedUp(4);
+//      bike.applyBrakes(3);
+//        
+//      System.out.println("Bike present state :");
+//      bike.printStates();
+//  }
+//}
