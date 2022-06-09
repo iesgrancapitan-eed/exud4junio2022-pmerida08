81d0a8748d86fb4d2f3f3c1557824dc0b10d751a
pmerida <pablomerida03@gmail.com>
2022-06-09 09:11:19 +0200

    refactor: extraer superclase

--- com/PMV/examen/junio/Bike.java
+++ com/PMV/examen/junio/Bike.java
@@ -1,50 +1,8 @@
 package com.PMV.examen.junio;
 
 
-class Bike {  
-  private int speed;
-  private int gear;
-    
-  public int getSpeed() {
-    return speed;
-  }
-
-
-  public void setSpeed(int speed) {
-    this.speed = speed;
-  }
-
-
-  public int getGear() {
-    return gear;
-  }
-
-
-  public void setGear(int gear) {
-    this.gear = gear;
-  }
-
-
-  public void changeGear(int newGear){     
-      gear = newGear;
-  }
-    
-  
-  public void speedUp(int increment){
-      speed = speed + increment;
-  }
-    
-  public void applyBrakes(int decrement){     
-      speed = speed - decrement;
-  }
-    
-  public void printStates() {
-      System.out.println("speed: " + speed
-          + " gear: " + gear);
-  }
-
-
-@Override
+class Bike extends Vehicle {  
+  @Override
 public String toString() {
 	return "Bike [speed=" + speed + ", gear=" + gear + "]";
 }
